a63188ad187ce4edf4f347055dc3b5604dbef98f
Vladislav Botvin <darkvlados@gmail.com>
2026-07-05 20:01:07 +0300

    rm unused subs

--- src/main/vygga/subs.cljs
+++ src/main/vygga/subs.cljs
@@ -9,11 +9,6 @@
 ;; ---- Yggdrasil subscriptions ----
 
 (rf/reg-sub
- :yggstack/state
- (fn [db _]
-   (get db :yggstack)))
-
-(rf/reg-sub
  :yggstack/status
  (fn [db _]
    (get-in db [:yggstack :status])))
@@ -33,22 +28,12 @@
  (fn [db _]
    (get-in db [:yggstack :address])))
 
-(rf/reg-sub
- :yggstack/public-key
- (fn [db _]
-   (get-in db [:yggstack :public-key])))
-
 ;; ---- Messenger subscriptions ----
 
 (rf/reg-sub
  :messenger/contacts
  (fn [db _]
    (get-in db [:messenger :contacts])))
-
-(rf/reg-sub
- :messenger/contact
- (fn [db [_ id]]
-   (get-in db [:messenger :contacts id])))
 
 (rf/reg-sub
  :messenger/server-running
@@ -59,20 +44,6 @@
  :messenger/current-contact
  (fn [db _]
    (get-in db [:messenger :current-contact])))
-
-(rf/reg-sub
- :messenger/current-messages
- :<- [:messenger/current-contact]
- :<- [:messenger/contacts]
- (fn [[cid contacts] _]
-   (get-in contacts [cid :messages] [])))
-
-(rf/reg-sub
- :messenger/current-contact-info
- :<- [:messenger/current-contact]
- :<- [:messenger/contacts]
- (fn [[cid contacts] _]
-   (get contacts cid)))
 
 (rf/reg-sub
  :messenger/current-has-more
