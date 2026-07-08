f4542266b8eb8143f120ec3fa00af9124f3a5bd0
Vladislav Botvin <darkvlados@gmail.com>
2026-07-08 20:07:25 +0300

    fix chat

--- src/main/vygga/view.cljs
+++ src/main/vygga/view.cljs
@@ -262,67 +262,69 @@
     (let [cid @current-id
           c (get @contacts cid)
           msgs (:messages c [])]
-      [:> rn/View {:style {:flex 1 :background-color (:bg t)}}
-       [:> rn/View {:style {:padding-horizontal 16 :padding-vertical 12
-                            :border-bottom-width 1 :border-bottom-color (:border t)
-                            :flex-direction :row :align-items :center}}
-        [:> rn/Text {:style {:font-size 17 :font-weight :600 :flex 1 :color (:text-primary t)}}
-         (or (:address c) "Unknown")]]
-       (if (empty? msgs)
-         [:> rn/View {:style {:flex 1 :padding 40 :align-items :center}}
-          [:> rn/Text {:style {:font-size 15 :color (:empty-text t)}}
-           "No messages yet"]]
-         [:> rn/FlatList
-          {:data (to-array (rseq msgs))
-           :key-extractor (fn [item] (:id item))
-           :inverted true
-           :ref #(reset! *flat-ref %)
-           :style {:flex 1 :padding 12}
-           :on-content-size-change (fn []
-                                     (when (and @*at-bottom @*flat-ref)
-                                       (try (.scrollToEnd @*flat-ref #js {:animated false})
-                                            (catch js/Error _))))
-           :on-scroll (fn [e]
-                        (let [offset (.-y (.-contentOffset e))]
-                          (reset! *at-bottom (< offset 50))))
-           :initial-num-to-render 20
-           :max-to-render-per-batch 20
-           :window-size 7
-           :render-item (fn [info]
-                          (let [item (.-item info)
-                                id (:id item)
-                                text (:text item)
-                                from-me (:from-me item)
-                                status (:status item)]
-                            (r/as-element
-                             [message-bubble
-                              {:id id :text text :from-me from-me
-                               :status status :cid cid}])))}])
-       [:> rn/View {:style {:flex-direction :row :align-items :center
-                            :padding 12 :border-top-width 1
-                            :border-top-color (:border t)
-                            :padding-bottom 24}}
-        [:> rn/TextInput {:style {:flex 1 :border-width 1 :border-color (:border-input-alt t)
-                                  :border-radius 20 :padding-horizontal 16
-                                  :padding-vertical 12 :font-size 15 :margin-right 8
-                                  :color (:text-primary t)}
-                          :placeholder "Type a message..."
-                          :placeholder-text-color (:text-tertiary t)
-                          :value @*text
-                          :on-change-text #(reset! *text %)
-                          :on-submit-editing #(when (seq @*text)
-                                                (rf/dispatch [:messenger/send-message cid @*text])
-                                                (reset! *text ""))}]
-        [:> rn/Pressable {:style {:width 40 :height 40 :border-radius 20
-                                  :background-color (:accent t) :justify-content :center
-                                  :align-items :center}
-                          :on-press (fn []
-                                      (let [t @*text]
-                                        (when (seq t)
-                                          (rf/dispatch [:messenger/send-message cid t])
-                                          (reset! *text ""))))}
-         [:> rn/Text {:style {:color (:text-inverse t) :font-size 18}} "➤"]]]
-       [:> StatusBar {:style "auto"}]])))
+      [:> rn/KeyboardAvoidingView {:style {:flex 1} :behavior "padding" :keyboard-vertical-offset 90}
+       [:> rn/View {:style {:flex 1 :background-color (:bg t)}}
+        [:> rn/View {:style {:padding-horizontal 16 :padding-vertical 12
+                             :border-bottom-width 1 :border-bottom-color (:border t)
+                             :flex-direction :row :align-items :center}}
+         [:> rn/Text {:style {:font-size 17 :font-weight :600 :flex 1 :color (:text-primary t)}}
+          (or (:address c) "Unknown")]]
+        (if (empty? msgs)
+          [:> rn/View {:style {:flex 1 :padding 40 :align-items :center}}
+           [:> rn/Text {:style {:font-size 15 :color (:empty-text t)}}
+            "No messages yet"]]
+          [:> rn/FlatList
+           {:data (to-array (rseq msgs))
+            :key-extractor (fn [item] (:id item))
+            :inverted true
+            :ref #(reset! *flat-ref %)
+            :style {:flex 1 :padding 12}
+            :on-content-size-change (fn []
+                                      (when (and @*at-bottom @*flat-ref)
+                                        (try (.scrollToEnd @*flat-ref #js {:animated false})
+                                             (catch js/Error _))))
+            :on-scroll (fn [e]
+                         (when-let [y (some-> e .-nativeEvent .-contentOffset .-y)]
+                           (reset! *at-bottom (< y 50))))
+            :initial-num-to-render 20
+            :max-to-render-per-batch 20
+            :window-size 7
+            :render-item (fn [info]
+                           (let [item (.-item info)
+                                 id (:id item)
+                                 text (:text item)
+                                 from-me (:from-me item)
+                                 status (:status item)]
+                             (r/as-element
+                              [message-bubble
+                               {:id id :text text :from-me from-me
+                                :status status :cid cid}])))}])
+        [:> rn/View {:style {:flex-direction :row :align-items :center
+                             :padding 12 :border-top-width 1
+                             :border-top-color (:border t)
+                             :padding-bottom 48}}
+         [:> rn/TextInput {:style {:flex 1 :border-width 1 :border-color (:border-input-alt t)
+                                   :border-radius 20 :padding-horizontal 16
+                                   :padding-vertical 14 :font-size 15 :margin-right 8
+                                   :min-height 44 :max-height 120 :color (:text-primary t)}
+                           :placeholder "Type a message..."
+                           :placeholder-text-color (:text-tertiary t)
+                           :multiline true
+                           :value @*text
+                           :on-change-text #(reset! *text %)
+                           :on-submit-editing #(when (seq @*text)
+                                                 (rf/dispatch [:messenger/send-message cid @*text])
+                                                 (reset! *text ""))}]
+         [:> rn/Pressable {:style {:width 40 :height 40 :border-radius 20
+                                   :background-color (:accent t) :justify-content :center
+                                   :align-items :center}
+                           :on-press (fn []
+                                       (let [t @*text]
+                                         (when (seq t)
+                                           (rf/dispatch [:messenger/send-message cid t])
+                                           (reset! *text ""))))}
+          [:> rn/Text {:style {:color (:text-inverse t) :font-size 18}} "➤"]]]
+        [:> StatusBar {:style "auto"}]]])))
 
 (defn root []
   (r/with-let [!root-state (rf/subscribe [:navigation/root-state])
